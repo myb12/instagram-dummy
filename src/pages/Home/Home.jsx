21fb3d38e749bb1e278267f70cf78d8c2c2f8a6a
myb12 <mohammadyasinbappy@outlook.com>
2022-07-27 23:52:33 +0600

    Header component is built

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+import Header from '../../component/Header/Header'
+
+const Home = () => {
+    return (
+        <div className='custom-container'>
+            <Header />
+        </div>
+    );
+};
+
+export default Home;
